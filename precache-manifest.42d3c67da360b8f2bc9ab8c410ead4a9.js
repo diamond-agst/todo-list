07dd1c7d77f0d097d63f82db11583149a880f217
diamond-agst <diamondagst@gmail.com>
2020-10-06 00:03:06 +0300

    Updates

---
+++ precache-manifest.42d3c67da360b8f2bc9ab8c410ead4a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2159ba43fffe290ebcd8cf7e7fbc8fa",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "d207a47f758893e06fcd",
+    "url": "/todo-list/static/css/main.4b4e0efe.chunk.css"
+  },
+  {
+    "revision": "e6bd7b827af3f251cf7a",
+    "url": "/todo-list/static/js/2.d24532fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.d24532fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d207a47f758893e06fcd",
+    "url": "/todo-list/static/js/main.f6fdcd60.chunk.js"
+  },
+  {
+    "revision": "27e019a4eb32999bd0bf",
+    "url": "/todo-list/static/js/runtime-main.88bd720a.js"
+  }
+]);
